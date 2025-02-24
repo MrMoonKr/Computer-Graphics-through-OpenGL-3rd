9436721d97f7636273d46487d896b644541bca3f
Mr.Moon <MrMoonKr@gmail.com>
2025-02-24 16:02:32 +0900

    개발 환경 구축 및 동작 테스트 ...

---
+++ src/Chapter00/ch00-02.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, OpenGL!, 개발환경구축 완료" << std::endl;
+    std::cout << "Chapter00-02 예제입니다" << std::endl;
+    return 0;
+}
---
+++ src/Chapter00/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, OpenGL!, 개발환경구축 완료" << std::endl;
+    return 0;
+}
